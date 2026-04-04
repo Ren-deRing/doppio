f12e1e67b3ed5ca3602ada0b9f4925209209977d
Ren-deRing <heebb0912.b@gmail.com>
2026-04-05 03:53:18 +0900

    blue white screen's revive cause i'm bored

--- kernel/generic.c
+++ kernel/generic.c
@@ -35,6 +35,25 @@ void generic_entry() {
     ap_release = true;
     __sync_synchronize();
 
+    uint32_t* fb_ptr = (uint32_t*)g_boot_info.fb.fb_addr;
+    uint32_t width = g_boot_info.fb.width;
+    uint32_t height = g_boot_info.fb.height;
+    uint32_t pixels_per_line = g_boot_info.fb.pitch / 4;
+
+    uint32_t test_color = 0x0077BE; 
+
+    for (uint32_t y = 0; y < height; y++) {
+        for (uint32_t x = 0; x < width; x++) {
+            fb_ptr[y * pixels_per_line + x] = test_color;
+        }
+    }
+
+    for (uint32_t y = height/2 - 50; y < height/2 + 50; y++) {
+        for (uint32_t x = width/2 - 50; x < width/2 + 50; x++) {
+            fb_ptr[y * pixels_per_line + x] = 0xFFFFFF;
+        }
+    }
+
     arch_irq_enable();
 
     for (;;) arch_halt();
